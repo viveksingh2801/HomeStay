cfb46a5a51e9898e7c3e1434ba0a01c5ddc64f24
Vivek Singh <vivek280197@gmail.com>
2024-08-13 11:29:58 +0530

    comment root route

--- app.js
+++ app.js
@@ -67,9 +67,9 @@ const sessionOptions = {
   },
 };
 
-app.get("/", (req, res) => {
-  res.send("Hi, I am root");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hi, I am root");
+// });
 
 app.use(session(sessionOptions));
 app.use(flash());
